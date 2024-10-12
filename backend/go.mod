329830b2a407244ec4e539e1703fe4691728ce4a
arthur-teixeira <arthurcarvalhot@yahoo.com.br>
2024-10-12 15:48:16 -0300

    Beggining of JWT session implementation

--- backend/go.mod
+++ backend/go.mod
@@ -5,6 +5,7 @@ go 1.22.1
 require github.com/lib/pq v1.10.9
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/rs/cors v1.11.1 // indirect
 	golang.org/x/crypto v0.28.0 // indirect
---
+++ backend/service/JWTValidator.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"time"
+	"github.com/golang-jwt/jwt"
+)
+
+var SECRET = []byte("VERY_SECRET_SECRET")
+
+func CreateJWTToken(name, email string) (string, error) {
+	claims := jwt.MapClaims{
+		"name":  name,
+		"email": email,
+		"exp":   time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(SECRET)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func ValidateToken(tokenString string) bool {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return SECRET, nil
+	})
+
+	if err != nil {
+		return false
+	}
+
+	return token.Valid
+}
